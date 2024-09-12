c734972af149045056e4873f58ed05e89b628837
sodamkimkim <theka265@gmail.com>
2024-09-12 18:19:28 +0900

    BuildPuzzle & SetPuzzlePivotToChildCenter

--- Assets/Scripts/BuildPuzzlePrefab.cs
+++ Assets/Scripts/BuildPuzzlePrefab.cs
@@ -16,10 +16,10 @@ public class BuildPuzzlePrefab : MonoBehaviour
         GameObject _puzzlePrefab_UpperLine = null;
         GameObject _puzzlePrefab_LeftUpperLine = null;
         GameObject _puzzlePrefab_NoLine = null;
-        _puzzlePrefab_leftLine = Resources.Load<GameObject>("Prefabs/Puzzle/Puzzle_LeftLine");
-        _puzzlePrefab_UpperLine = Resources.Load<GameObject>("Prefabs/Puzzle/Puzzle_UpperLine");
-        _puzzlePrefab_LeftUpperLine = Resources.Load<GameObject>("Prefabs/Puzzle/Puzzle_LeftUpperLine");
-        _puzzlePrefab_NoLine = Resources.Load<GameObject>("Prefabs/Puzzle/Puzzle_NoLine");
+        _puzzlePrefab_leftLine = Resources.Load<GameObject>("Prefabs/BuildPuzzle/Puzzle_LeftLine");
+        _puzzlePrefab_UpperLine = Resources.Load<GameObject>("Prefabs/BuildPuzzle/Puzzle_UpperLine");
+        _puzzlePrefab_LeftUpperLine = Resources.Load<GameObject>("Prefabs/BuildPuzzle/Puzzle_LeftUpperLine");
+        _puzzlePrefab_NoLine = Resources.Load<GameObject>("Prefabs/BuildPuzzle/Puzzle_NoLine");
 
         for (int i = 0; i < _puzzleManager.PuzzleArr.Length; i++)
         {
@@ -37,25 +37,57 @@ public class BuildPuzzlePrefab : MonoBehaviour
                         GameObject puzzlePartPrefab = null;
                         if (r == 0 && c == 0)
                             puzzlePartPrefab = _puzzlePrefab_NoLine;
-                        else 
+                        else
                         {
                             if (r == 0 && c != 0)
                                 puzzlePartPrefab = _puzzlePrefab_leftLine;
-                            else if(r!=0 && c==0)
+                            else if (r != 0 && c == 0)
                                 puzzlePartPrefab = _puzzlePrefab_UpperLine;
                             else
                                 puzzlePartPrefab = _puzzlePrefab_LeftUpperLine;
                         }
 
                         GameObject puzzlePart = Instantiate(puzzlePartPrefab, puzzlePrefab.transform);
+                        puzzlePart.name = $"PuzzlePart_R{r}_C{c}";
                         puzzlePart.transform.rotation = Quaternion.identity;
                         puzzlePart.transform.localPosition = new Vector3(puzzlePart.transform.localScale.x * c, -puzzlePart.transform.localScale.y * r, 0);
                     }
 
                 }
             }
-            // puzzlePrefab.transform.localScale = new Vector3(1f,1f, 1f);
+            puzzlePrefab.transform.localScale = new Vector3(1f, 1f, 1f);
             puzzlePrefab.transform.rotation = Quaternion.identity;
+            SetPivotToChildCenter(puzzlePrefab.transform);
+            puzzlePrefab.transform.position = new Vector3(i*puzzlePrefab.transform.localScale.x * 4f, -3f, 0f);
+        }
+    }
+    private void SetPivotToChildCenter(Transform puzzlePrefabTr)
+    {
+        if (puzzlePrefabTr.childCount == 0)
+        {
+            Debug.LogWarning("This GameObject has no children.");
+            return;
+        }
+
+        // 자식들의 월드 좌표를 합산하여 중심 계산
+        Vector3 center = Vector3.zero;
+        foreach (Transform child in puzzlePrefabTr)
+        {
+            center += child.position;
+        }
+        center /= puzzlePrefabTr.childCount;
+
+        // 부모의 위치를 중심으로 이동하고, 자식들의 위치를 새로운 부모 위치에 맞게 조정
+        Vector3 parentPos = puzzlePrefabTr.position;
+        Vector3 offset = parentPos - center;
+
+        // 부모 오브젝트를 자식들의 중심으로 이동
+        puzzlePrefabTr.position = center;
+
+        // 자식들의 위치를 새로운 부모 위치에 맞게 이동
+        foreach (Transform child in puzzlePrefabTr)
+        {
+            child.position += offset;
         }
     }
 } // end of class
