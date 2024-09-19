4e3bc7af66c455b22d783d6de0642e8bcab0e26c
sodamkimkim <theka265@gmail.com>
2024-09-19 16:42:11 +0900

    .

--- Assets/Scripts/BaseSpritePool.cs
+++
@@ -1,5 +0,0 @@
-using UnityEngine;
-
-public abstract class BaseSpritePool : MonoBehaviour
-{ 
-} // end of class
---
+++ Assets/Scripts/Unit/Puzzle.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class Puzzle : MonoBehaviour
+{
+ 
+} // end of class
