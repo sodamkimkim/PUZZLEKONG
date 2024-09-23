047b3a81da719003e2254105c0494a04dfbd9f72
sodamkimkim <theka265@gmail.com>
2024-09-23 15:13:49 +0900

    GridArrayResource

--- Assets/Scripts/Utils/Path.cs
+++ Assets/Scripts/Utils/Path.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Path
 {
-    public string Backgrounds { get => "Prefabs/BackGround/"; }
-    public string Puzzles { get => "Prefabs/Puzzles/"; }
-    public string PuzzlePartPrefab { get => "Prefabs/BuildPuzzle/Puzzle_NoLine"; }
+    public static string Backgrounds { get => "Prefabs/BackGround/"; }
+    public static string Puzzles { get => "Prefabs/Puzzles/"; }
+    public static string PuzzlePartPrefab { get => "Prefabs/BuildPuzzle/Puzzle_NoLine"; }
 } // end of class
