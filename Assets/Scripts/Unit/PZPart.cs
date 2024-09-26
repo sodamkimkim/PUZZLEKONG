7a2965112fcaa233e4b357fedb17c199d3a226a4
sodamkimkim <theka265@gmail.com>
2024-09-26 15:45:53 +0900

    Update PZPart.cs

--- Assets/Scripts/Unit/PZPart.cs
+++ Assets/Scripts/Unit/PZPart.cs
@@ -4,23 +4,19 @@ public class PZPart : MonoBehaviour
 {
     private bool _isInGrid;
     private Puzzle _parentPuzzle;
-    public bool IsInGrid { get => _isInGrid; private set => _isInGrid = value; } 
+    public bool IsInGrid { get => _isInGrid; private set => _isInGrid = value; }
     public Puzzle ParentPuzzle { get => _parentPuzzle; set => _parentPuzzle = value; }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Grid")
-        {
-            IsInGrid = true;
-            ParentPuzzle.HandleCallbackChildPuzzlePartCollision(IsInGrid); 
-        }
+        if (collision.tag != "Grid") return;
+        IsInGrid = true;
+        ParentPuzzle.HandleCallbackChildPuzzlePartCollision(IsInGrid);
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.tag == "Grid")
-        {
-            IsInGrid = false;
-            ParentPuzzle.HandleCallbackChildPuzzlePartCollision(IsInGrid); 
-        }
+        if (collision.tag != "Grid") return;
+        IsInGrid = false;
+        ParentPuzzle.HandleCallbackChildPuzzlePartCollision(IsInGrid);
     }
 } // end of class
